c3205f05a02d477113fcbf2776c50c81dce161f3
Noam SCHMITT <noam.schmitt@telecom-sudparis.eu>
2024-07-05 21:48:55 +0200

    Feature: substitutions customization, replaced default substitutions, removed HTML tags

--- src/main.ts
+++ src/main.ts
@@ -1,214 +1,256 @@
-import { Editor, MarkdownView, Notice, Plugin } from "obsidian";
-import { SearchCursor } from "src/search";
+import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
+import { SearchCursor } from 'src/search';
+import { EnhancedSymbolsPrettifierSettingsTab } from './settings';
+import { DEFAULT_SETTINGS, Settings } from './defaultSettings';
 
-interface HTMLObject {
-  transform: string;
-  classes: string;
-  element: string;
-}
+// interface HTMLObject {
+// 	transform: string;
+// 	classes: string;
+// 	element: string;
+// }
 
-interface CharacterMap {
-  [key: string]: string | HTMLObject;
-}
 
-const characterMap: CharacterMap = {
-  "->": "→",
-  "<-": "←",
-  "<->": "↔",
-  "<=>": "⇔",
-  "<=": "⇐",
-  "=>": "⇒",
-  "--": "–",
-  "!important": {
-    transform: "!important",
-    classes: "symbols-prettifier-important",
-    element: '<span class="symbols-prettifier-important">!important</span>',
-  },
-  "?unclear": {
-    transform: "?unclear",
-    classes: "symbols-prettifier-unclear",
-    element: '<span class="symbols-prettifier-unclear">?unclear</span>',
-  },
-};
+export default class EnhancedSymbolsPrettifier extends Plugin {
+	settings: Settings;
+	async onload() {
+		await this.loadSettings();
 
-export default class SymbolsPrettifier extends Plugin {
-  onload() {
-    console.log("loading symbols prettifier");
+		this.addSettingTab(
+			new EnhancedSymbolsPrettifierSettingsTab(this.app, this)
+		);
 
-    this.addCommand({
-      id: "symbols-prettifier-add-important",
-      name: "Add important symbol",
-      editorCallback: (editor) => {
-        const cursor = editor.getCursor();
-        const symbol = characterMap["!important"];
-        if (typeof symbol !== "string") {
-          editor.replaceRange(symbol.element, cursor);
-        }
-      },
-    });
+		// this.addCommand({
+		//   id: "symbols-prettifier-add-important",
+		//   name: "Add important symbol",
+		//   editorCallback: (editor) => {
+		//     const cursor = editor.getCursor();
+		//     const symbol = characterMapDefault["!important"];
+		//     if (typeof symbol !== "string") {
+		//       editor.replaceRange(symbol.element, cursor);
+		//     }
+		//   },
+		// });
 
-    this.addCommand({
-      id: "symbols-prettifier-add-unclear",
-      name: "Add unclear symbol",
-      editorCallback: (editor) => {
-        const cursor = editor.getCursor();
-        const symbol = characterMap["?unclear"];
-        if (typeof symbol !== "string") {
-          editor.replaceRange(symbol.element, cursor);
-        }
-      },
-    });
+		// this.addCommand({
+		//   id: "symbols-prettifier-add-unclear",
+		//   name: "Add unclear symbol",
+		//   editorCallback: (editor) => {
+		//     const cursor = editor.getCursor();
+		//     const symbol = characterMapDefault["?unclear"];
+		//     if (typeof symbol !== "string") {
+		//       editor.replaceRange(symbol.element, cursor);
+		//     }
+		//   },
+		// });
 
-    this.addCommand({
-      id: "symbols-prettifier-format-symbols",
-      name: "Prettify existing symbols in document",
-      editorCallback: (editor) => {
-        let value = editor.getValue();
-        const codeBlocks = this.getCodeBlocks(value);
-        let matchedChars: { from: number; to: number }[] = [];
+		this.addCommand({
+			id: 'symbols-prettifier-format-symbols',
+			name: 'Prettify existing symbols in document',
+			editorCallback: (editor) => this.prettifyInDocument(editor),
+		});
 
-        const matchChars = Object.entries(characterMap).reduce(
-          (prev, [curr]) => {
-            if (prev.length === 0) {
-              return prev + this.escapeRegExp(curr);
-            }
-            return prev + "|" + this.escapeRegExp(curr);
-          },
-          "",
-        );
+		this.registerDomEvent(document, 'keydown', (event: KeyboardEvent) => {
+			this.keyDownEvent(event);
+		});
+	}
 
-        const searchCursor = new SearchCursor(
-          value,
-          new RegExp("(?<![\\w\\d])" + matchChars + "(?![\\w\\d])"),
-          0,
-        );
-        while (searchCursor.findNext() !== undefined) {
-          matchedChars.push({
-            from: searchCursor.from(),
-            to: searchCursor.to(),
-          });
-        }
+	onunload() {}
 
-        matchedChars = matchedChars.filter((matchedChar) => {
-          return !codeBlocks.some(
-            (cb) => cb.from <= matchedChar.from && cb.to >= matchedChar.to,
-          );
-        });
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+		this.settings.replacements = Object.assign(
+			{},
+			DEFAULT_SETTINGS.replacements,
+			this.settings.replacements
+		);
+	}
 
-        let diff = 0;
-        matchedChars.forEach((matchedChar) => {
-          const symbol = value.substring(
-            matchedChar.from - diff,
-            matchedChar.to - diff,
-          );
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 
-          const character = characterMap[symbol];
-          if (typeof character === "string") {
-            value =
-              value.substring(0, matchedChar.from - diff) +
-              character +
-              value.substring(matchedChar.to - diff);
-            diff += symbol.length - character.length;
-          } else {
-            value =
-              value.substring(0, matchedChar.from - diff) +
-              character.element +
-              value.substring(matchedChar.to - diff);
-            diff += symbol.length - character.element.length;
-          }
-        });
+	async restoreDefaultSettings() {
+		this.settings = JSON.parse(JSON.stringify(DEFAULT_SETTINGS));
+		await this.saveSettings();
+	}
 
-        editor.setValue(value);
-      },
-    });
+	private prettifyInDocument(editor: Editor) {
+		let value = editor.getValue();
+		const codeBlocks = this.getCodeBlocks(value);
+		let matchedChars: { from: number; to: number }[] = [];
 
-    this.registerDomEvent(document, "keydown", (event: KeyboardEvent) => {
-      const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const matchChars = Object.entries(this.settings.replacements).reduce(
+			(prev, [curr]) => {
+				if (prev.length === 0) {
+					return prev + this.escapeRegExp(curr);
+				} else if (
+					curr.length === 0 ||
+					this.settings.replacements[curr].disabled
+				) {
+					return prev;
+				}
+				return prev + '|' + this.escapeRegExp(curr);
+			},
+			''
+		);
 
-      if (view) {
-        const cursor = view.editor.getCursor();
-        if (event.key === " ") {
-          const line = view.editor.getLine(cursor.line);
-          let from = -1;
-          let sequence = "";
-          for (let i = cursor.ch - 1; i >= 0; i--) {
-            if (line.charAt(i) === " ") {
-              const excludeWhitespace = i + 1;
-              from = excludeWhitespace;
-              sequence = line.slice(excludeWhitespace, cursor.ch);
-              break;
-            } else if (i === 0) {
-              from = i;
-              sequence = line.slice(i, cursor.ch);
-              break;
-            }
-          }
-          const replaceCharacter = characterMap[sequence];
-          if (
-            replaceCharacter &&
-            from !== -1 &&
-            typeof replaceCharacter !== "function" &&
-            !this.isCursorInUnwantedBlocks(view.editor)
-          ) {
-            if (typeof replaceCharacter === "string") {
-              view.editor.replaceRange(
-                replaceCharacter,
-                { line: cursor.line, ch: from },
-                { line: cursor.line, ch: cursor.ch },
-              );
-            } else {
-              view.editor.replaceRange(
-                `<span class="${replaceCharacter.classes}">${replaceCharacter.transform}</span>`,
-                { line: cursor.line, ch: from },
-                { line: cursor.line, ch: cursor.ch },
-              );
-            }
-          }
-        }
-      }
-    });
-  }
+		const searchCursor = new SearchCursor(
+			value,
+			new RegExp('(?<![\\w\\d])' + matchChars + '(?![\\w\\d])'),
+			0
+		);
+		while (searchCursor.findNext() !== undefined) {
+			matchedChars.push({
+				from: searchCursor.from(),
+				to: searchCursor.to(),
+			});
+		}
 
-  onunload() {
-    console.log("unloading symbols prettifier");
-  }
+		matchedChars = matchedChars.filter((matchedChar) => {
+			return !codeBlocks.some(
+				(cb) => cb.from <= matchedChar.from && cb.to >= matchedChar.to
+			);
+		});
 
-  private escapeRegExp(string: string): string {
-    return string.replace(/[.*+?^!${}()|[\]\\]/g, "\\$&");
-  }
+		let diff = 0;
+		let replacementsCount = 0;
+		matchedChars.forEach((matchedChar) => {
+			const symbol = value.substring(
+				matchedChar.from - diff,
+				matchedChar.to - diff
+			);
 
-  private getCodeBlocks(input: string) {
-    const result: { from: number; to: number }[] = [];
+			if (!this.settings.replacements[symbol]) {
+				return;
+			}
 
-    const codeBlock = /```\w*[^`]+```/;
-    const searchCursor = new SearchCursor(input, codeBlock, 0);
+			const replacement = this.settings.replacements[symbol];
 
-    while (searchCursor.findNext() !== undefined) {
-      result.push({ from: searchCursor.from(), to: searchCursor.to() });
-    }
+			if (replacement.disabled) {
+				return;
+			}
 
-    return result;
-  }
+			const character = replacement.value;
 
-  private isCursorInUnwantedBlocks(editor: Editor): boolean {
-    const unwantedBlocks = [/(?<!`)`[^`\n]+`(?!`)/, /```\w*\s*[\s\S]*?```/]; // inline code, full code
+			if (typeof character === 'string') {
+				value =
+					value.substring(0, matchedChar.from - diff) +
+					character +
+					value.substring(matchedChar.to - diff);
+				diff += symbol.length - character.length;
+			} 
+      // else {
+			// 	value =
+			// 		value.substring(0, matchedChar.from - diff) +
+			// 		character.element +
+			// 		value.substring(matchedChar.to - diff);
+			// 	diff += symbol.length - character.element.length;
+			// }
+			replacementsCount++;
+		});
 
-    return (
-      unwantedBlocks.filter((unwantedBlock) => {
-        const searchCursor = new SearchCursor(
-          editor.getValue(),
-          unwantedBlock,
-          0,
-        );
-        while (searchCursor.findNext() !== undefined) {
-          const offset = editor.posToOffset(editor.getCursor());
-          if (searchCursor.from() <= offset && searchCursor.to() >= offset) {
-            return true;
-          }
-        }
+		editor.setValue(value);
+		new Notice(`Replaced ${replacementsCount} symbol.s`);
+	}
 
-        return false;
-      }).length !== 0
-    );
-  }
+	private keyDownEvent(event: KeyboardEvent) {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		if (view) {
+			const cursor = view.editor.getCursor();
+			if (event.key === ' ') {
+				const line = view.editor.getLine(cursor.line);
+				let from = -1;
+				let sequence = '';
+				for (let i = cursor.ch - 1; i >= 0; i--) {
+					if (line.charAt(i) === ' ') {
+						const excludeWhitespace = i + 1;
+						from = excludeWhitespace;
+						sequence = line.slice(excludeWhitespace, cursor.ch);
+						break;
+					} else if (i === 0) {
+						from = i;
+						sequence = line.slice(i, cursor.ch);
+						break;
+					}
+				}
+				const replacement = this.settings.replacements[sequence];
+				if (!replacement) {
+					return;
+				}
+				if (replacement.disabled) {
+					return;
+				}
+				const replaceCharacter = replacement.value;
+				if (
+					replaceCharacter &&
+					sequence.length > 0 &&
+					from !== -1 &&
+					typeof replaceCharacter !== 'function' &&
+					!this.isCursorInUnwantedBlocks(view.editor)
+				) {
+					if (typeof replaceCharacter === 'string') {
+						view.editor.replaceRange(
+							replaceCharacter,
+							{ line: cursor.line, ch: from },
+							{ line: cursor.line, ch: cursor.ch }
+						);
+					} 
+          // else {
+					// 	view.editor.replaceRange(
+					// 		`<span class="${replaceCharacter.classes}">${replaceCharacter.transform}</span>`,
+					// 		{ line: cursor.line, ch: from },
+					// 		{ line: cursor.line, ch: cursor.ch }
+					// 	);
+					// }
+				}
+			}
+		}
+	}
+
+	private escapeRegExp(string: string): string {
+		return string.replace(/[.*+?^!${}()|[\]\\]/g, '\\$&');
+	}
+
+	private getCodeBlocks(input: string) {
+		const result: { from: number; to: number }[] = [];
+
+		const codeBlock = /```\w*[^`]+```/;
+		const searchCursor = new SearchCursor(input, codeBlock, 0);
+
+		while (searchCursor.findNext() !== undefined) {
+			result.push({ from: searchCursor.from(), to: searchCursor.to() });
+		}
+
+		return result;
+	}
+
+	private isCursorInUnwantedBlocks(editor: Editor): boolean {
+		const unwantedBlocks = [/(?<!`)`[^`\n]+`(?!`)/, /```\w*\s*[\s\S]*?```/]; // inline code, full code
+
+		return (
+			unwantedBlocks.filter((unwantedBlock) => {
+				const searchCursor = new SearchCursor(
+					editor.getValue(),
+					unwantedBlock,
+					0
+				);
+				while (searchCursor.findNext() !== undefined) {
+					const offset = editor.posToOffset(editor.getCursor());
+					if (
+						searchCursor.from() <= offset &&
+						searchCursor.to() >= offset
+					) {
+						return true;
+					}
+				}
+
+				return false;
+			}).length !== 0
+		);
+	}
 }
---
+++ src/settings.ts
@@ -0,0 +1,169 @@
+import { Substitution } from './defaultSettings';
+import EnhancedSymbolsPrettifier from './main';
+import { PluginSettingTab, App, Setting } from 'obsidian';
+
+export class EnhancedSymbolsPrettifierSettingsTab extends PluginSettingTab {
+	plugin: EnhancedSymbolsPrettifier;
+
+	constructor(app: App, plugin: EnhancedSymbolsPrettifier) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	displayReplacement(
+		replacement: Substitution,
+		i: number,
+		containerEl: HTMLElement
+	): void {
+		const value = replacement.value;
+		let key = replacement.replaced;
+		new Setting(containerEl)
+			.setName(`${i}`)
+			.addText((text) =>
+				text
+					.setPlaceholder('To replace')
+					.setValue(key)
+					.onChange(async (index) => {
+						this.plugin.settings.replacements[key].replaced = index;
+						this.plugin.settings.replacements[index] =
+							this.plugin.settings.replacements[key];
+						delete this.plugin.settings.replacements[key];
+						key = index;
+						await this.plugin.saveSettings();
+					})
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder('Replace with')
+					.setValue(value)
+					.onChange(async (val) => {
+						this.plugin.settings.replacements[key].value = val;
+						await this.plugin.saveSettings();
+					})
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(!replacement.disabled)
+					.onChange(async (value) => {
+						this.plugin.settings.replacements[key].disabled =
+							!value;
+						await this.plugin.saveSettings();
+					})
+			)
+			.addButton((button) =>
+				button.setIcon('x').onClick(async () => {
+					delete this.plugin.settings.replacements[key];
+					await this.plugin.saveSettings();
+					this.display();
+				})
+			);
+	}
+
+	displayGroup(group: string, containerEl: HTMLElement): void {
+		containerEl.createEl('h3', {
+			text: group,
+		});
+
+		new Setting(containerEl).setName('Disable group').addToggle((toggle) =>
+			toggle
+				.setValue(
+					Object.values(this.plugin.settings.replacements)
+						.filter((replacement) => replacement.group === group)
+						.filter((replacement) => !replacement.disabled)
+						.length === 0
+				)
+				.onChange(async (value) => {
+					for (const key in this.plugin.settings.replacements) {
+						const replacement =
+							this.plugin.settings.replacements[key];
+						if (replacement.group === group) {
+							this.plugin.settings.replacements[key].disabled =
+								value;
+						}
+					}
+					await this.plugin.saveSettings();
+					this.display();
+				})
+		);
+
+		let i = 0;
+		for (const key in this.plugin.settings.replacements) {
+			const replacement = this.plugin.settings.replacements[key];
+			if (replacement.group === group) {
+				i++;
+				this.displayReplacement(replacement, i, containerEl);
+			}
+		}
+
+		new Setting(containerEl).setName('Add new symbol').addButton((button) =>
+			button.setButtonText('Add').onClick(async () => {
+				this.plugin.settings.replacements[''] = {
+					replaced: '',
+					value: '',
+					disabled: false,
+					group: group,
+				};
+				await this.plugin.saveSettings();
+				this.display();
+			})
+		);
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+
+		containerEl.createEl('p', {
+			text: 'Add or remove symbols to prettify in your notes. You can also temporarily disable a symbol by toggling the switch.',
+		});
+
+		const groups = new Set<string>();
+		for (const key in this.plugin.settings.replacements) {
+			const replacement = this.plugin.settings.replacements[key];
+			groups.add(replacement.group);
+		}
+
+		for (const group of groups) {
+			this.displayGroup(group, containerEl);
+		}
+
+		containerEl.createEl('hr');
+
+		let textNewGroup = '';
+		new Setting(containerEl)
+			.setName('Add new group')
+			.addText((text) =>
+				text.setPlaceholder('Group name').onChange(async (value) => {
+					textNewGroup = value;
+				})
+			)
+			.addButton((button) =>
+				button.setButtonText('Add').onClick(async () => {
+					const groupName = textNewGroup;
+					if (groupName) {
+						this.plugin.settings.replacements[''] = {
+							replaced: '',
+							value: '',
+							disabled: false,
+							group: groupName,
+						};
+						await this.plugin.saveSettings();
+						this.display();
+					}
+				})
+			);
+
+		new Setting(containerEl)
+			.setName('Reset to default')
+			.addButton((button) =>
+				button.setButtonText('Reset').onClick(async () => {
+					await this.plugin.restoreDefaultSettings();
+					this.display();
+				})
+			);
+
+		containerEl.createEl('p', {
+			text: 'Made by Noam Schmitt based on the Symbols Prettifier plugin by Florian Woelki.',
+		});
+	}
+}
